76714e75ed871e156b3ba8d6a69eb5561da4e2b0
Tamim Baschour <t.baschour@gmail.com>
2019-01-29 14:47:37 +0100

    force python2 for build

--- build-travis.sh
+++ build-travis.sh
@@ -2,9 +2,10 @@
 set -e
 set -x
 
+rm -rf win linux
 mkdir win
 mkdir linux
-python get-demoinfogo.py
+python2 get-demoinfogo.py
 mv demoinfogo.exe win/
 unzip demoinfogo-linux.zip -d linux
 demoinfogo_bin_win=win demoinfogo_bin_linux=linux python build.py
